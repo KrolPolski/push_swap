f02c825a94fe266927aa8880fb7cab0600762c0c
Ryan Boudwin <rboudwin@student.hive.fi>
2023-10-23 14:41:52 +0300

    ft_isalpha.c complete

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,27 +6,31 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:40:36 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/10/23 13:52:32 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/10/23 14:40:28 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isalpha(int c)
+// 65 - 90 is uppercase
+// 97 - 122 is lowercase
+int	ft_isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
-// 65 - 90 is uppercase
-// 97 - 122 is lowercase
 }
-
+/*
 #include <stdio.h>
+#include <ctype.h>
 int main(void)
 {
 	int result;
 	int c;
+	int checkresult;
 
-	c = 122;
+	c = 90;
 	result = ft_isalpha(c);
-	printf("isalpha(%d) returns the value of %d)", c, result);
-}
+	printf("ft_isalpha(%d) returns the value of %d\n", c, result);
+	checkresult = isalpha(c);
+	printf("isalpha(%d) returns the value of %d\n", c, checkresult);
+}*/
