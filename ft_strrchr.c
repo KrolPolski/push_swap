c9a29fd768e106ba41346ff97f052741d1a7d153
Ryan Boudwin <rboudwin@student.hive.fi>
2023-10-25 10:00:16 +0300

    fr_strrchr done, ft_strchr renamed

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,24 +6,39 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 18:34:25 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/10/25 09:57:42 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/10/25 09:59:48 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
 
-char	*ft_strchr(const char *s, int c)
+size_t static	ft_strlen(const char *s)
 {
-	int	i;
+	int		i;
+	size_t	length;
 
 	i = 0;
-	while (s[i] != c && s[i] != '\0')
+	length = 0;
+	while (s[i] != '\0')
+	{
+		length++;
 		i++;
+	}
+	return (length);
+}
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = ft_strlen(s) - 1;
+	while (s[i] != c && i != 0)
+		i--;
 	if (s[i] == c)
 		return ((char *)&s[i]);
 	else
 		return (NULL);
 }
-/*
+
 #include <stdio.h>
 int	main(void)
 {
@@ -32,6 +47,6 @@ int	main(void)
 	char *ptr;
 
 	c = 'i';
-	ptr = ft_strchr(string, c);
+	ptr = ft_strrchr(string, c);
 	printf("The character '%c' is found at: %p which is %c", c, ptr, *ptr); 
-}*/
+}
