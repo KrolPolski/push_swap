5acab6c40194881ea95e51e038bc0f5bf1737158
Ryan Boudwin <rboudwin@student.hive.fi>
2023-10-25 10:35:57 +0300

    ft_memchr.c complete

---
+++ ft_memchr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 10:21:52 by rboudwin          #+#    #+#             */
+/*   Updated: 2023/10/25 10:35:37 by rboudwin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = (char *)s;
+	while (str[i] != '\0' && str[i] != c && i < n)
+		i++;
+	if (str[i] == c)
+		return (&str[i]);
+	else
+		return (NULL);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char string[] = "So long and thanks";
+	char search = 'l';
+	void *ptr;
+
+	ptr = ft_memchr(string, search, 18);
+	printf("The character '%c' is found in '%s' at '%p'", search, string, ptr);
+}*/
