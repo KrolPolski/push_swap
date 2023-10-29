e9c47a966495cf23526febe5798d0eec843cf556
Ryan Boudwin <rboudwin@Mac-mini.local>
2023-10-29 17:57:06 +0200

    ft_strmapi.c in progress

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 14:23:25 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/10/27 14:44:00 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/10/29 17:53:25 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,21 @@
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
     char    *tmp_str;
+	int		i;
+	int		length;
 
-    tmp_str = malloc(ft_strlen(s) + 1);
-    return (tmp_str);
+ 	i = 0;
+	length = ft_strlen(s);
+    tmp_str = malloc(length + 1);
+	if (tmp_str == NULL)
+		return (NULL);
+    while (s[i] != '\0')
+{
+	tmp_str[i] = f(s[i]);
+	i++;
+}
+	tmp_str[i] = '\0';
+	return (tmp_str);
 
 }
 
