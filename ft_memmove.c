e536fbccc5e34fbe5dabc3f2765417d0c65c18e8
Ryan Boudwin <rboudwin@student.hive.fi>
2023-10-30 13:47:51 +0200

    fixed bugs in ft_memmove for overlap case

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,28 +6,33 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 14:26:37 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/10/25 15:55:33 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/10/30 13:47:13 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	void		*result;
 	size_t		i;
 	char		*destination;
 	const char	*source;
 
 	destination = dst;
 	source = src;
-	result = dst;
 	i = 0;
-	while (i < len)
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	while (dst < src && i < len)
 	{
 		destination[i] = source[i];
 		i++;
 	}
-	return (result);
+	while (dst > src && len > 0)
+	{
+		destination[len - 1] = source[len - 1];
+		len--;
+	}
+	return ((void *)destination);
 }
 /*
 #include <stdio.h>
