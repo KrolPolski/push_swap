5b38f52d4d4d205ab8f0e5d770f242e47389ca2f
Ryan Boudwin <rboudwin@student.hive.fi>
2023-10-30 15:42:53 +0200

    ft_itoa.c passing tests, needs norm

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 10:50:04 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/10/27 12:45:29 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/10/30 15:41:31 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -52,7 +52,20 @@ char	*ft_itoa(int n)
 	char	*str;
 
 	i = 0;
-	str = malloc(12);
+	digits = 1;
+	k = n;
+	while (k / 10 != 0)
+	{
+		digits++;
+		k = k / 10;
+	}
+	k = digits - 1;
+	if (n < 0)
+		str = malloc(digits + 2);
+	else
+		str = malloc(digits + 1);
+	if (str == NULL)
+		return (NULL);
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
 	if (n < 0)
@@ -60,14 +73,6 @@ char	*ft_itoa(int n)
 		str[i++] = '-';
 		n = -n;
 	}
-	k = n;
-	digits = 1;
-	while (k / 10 > 0)
-	{
-		digits++;
-		k = k / 10;
-	}
-	k = digits - 1;
 	return (ft_conversion(k, i, str, n));
 }
 /*
