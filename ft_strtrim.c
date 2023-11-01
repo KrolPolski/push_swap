fe690de2d51d7091adcd3b54d9c26e2ac1704a63
Ryan Boudwin <rboudwin@student.hive.fi>
2023-11-01 11:31:53 +0200

    ft_strtrim now passes unit tests

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 14:01:00 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/11/01 11:08:27 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/11/01 11:30:33 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,35 @@ unsigned char	*ft_strtrim(unsigned char const *s1, unsigned char const *set)
 	unsigned char	*clean_string;
 
 	i = 0;
+	if (s1 == NULL)
+		return (NULL);
 	j = ft_strlen(s1) - 1;
-	/*if (s1[0] == '\0')
+	if (s1[0] == '\0')
 	{
 		clean_string = malloc(1);
 		if (clean_string == NULL)
 			return (NULL);
-		else 
+		else
+		{
 			clean_string[0] = '\0';
 			return (clean_string);
+		}
 	}
 	if (set[0] == '\0')
-		return ((unsigned char *)s1);*/
+		return ((unsigned char *)s1);
 	while (s1[i] != '\0' && ft_strchr(set, s1[i]) != NULL)
 		i++;
 	while (j > 0 && ft_strchr(set, s1[j]) != NULL)
 		j--;
-	clean_string = malloc(j - i);
+	if (j < i)
+	{
+		clean_string = malloc(1);
+		if (clean_string == NULL)
+			return (NULL);
+		clean_string[0] = '\0';
+		return (clean_string);
+	}
+	clean_string = malloc(j - i + 2);
 	if (clean_string == NULL)
 		return (NULL);
 	k = 0;
@@ -44,6 +56,7 @@ unsigned char	*ft_strtrim(unsigned char const *s1, unsigned char const *set)
 	{
 		clean_string[k++] = s1[i++];
 	}
+	clean_string[k] = '\0';
 	return (clean_string);
 }
 /*
