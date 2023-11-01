1cd29679757f4332e49619de6290c766462ac193
Ryan Boudwin <rboudwin@student.hive.fi>
2023-11-01 14:33:30 +0200

    fixed an issue with incrementing j once too many times and looping around to 0

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,12 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 15:48:23 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/11/01 13:48:27 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/11/01 14:32:20 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+//#include <stdio.h>
 char	**ft_split(char const *s, char c)
 {
 	int		i;
@@ -36,21 +36,28 @@ char	**ft_split(char const *s, char c)
 			str_array[1][0] = '\0';
 			return str_array;
 		}
+//	while (s[i] == c)
+//		i++;
 	while (s[i] != '\0')
 	{
 		if (s[i] != c)
 			str_array[j][k++] = s[i];
+		//else if (s[i] == c && s[i + 1] != c && str_array[j][0] == '\0')
+
 		else if (s[i] == c && s[i + 1] != c && str_array[j][0] != '\0')
 		{
+//			printf("we are about to null terminate a string, j is now '%d' and the current string is '%s'\n", j, str_array[j]);
 			str_array[j][k] = '\0';
 			k = 0;
 			str_array[++j] = malloc(ft_strlen(s) + 1);
 		}
 		i++;
 	}
+//	printf("str_array[0] is currently '%s'", str_array[0]);
 	str_array[j][k] = '\0';
-	j++;
+//	printf("'%d' is current value of j", j);
 	str_array[j] = NULL;
+//	printf("str_array[0] is '%s' right before return", str_array[0]);
 	return (str_array);
 }
 /*
@@ -59,19 +66,21 @@ char	**ft_split(char const *s, char c)
 int	main(void)
 {
 //	char	str[] = ",,,So long,,, and thanks,,, for all, the fish,,,";
-	char    str[] = "   So-long    and-thanks    for-all    the-fish    ";
+//	char    str[] = "   So-long    and-thanks    for-all    the-fish    ";
 //	char	c = ',';
+	char str[] = "      split       this for   me  !       ";
 	char    c = ' ';
 	char	**str_array;
 
 	str_array = ft_split(str, c);
 	printf("'%s' split by '%c' results in:\n", str, c);
-	printf("'%s' '%s' '%s' '%s' \n", str_array[0], str_array[1],
-		str_array[2], str_array[3]);
+	printf("'%s' '%s' '%s' '%s' '%s' \n", str_array[0], str_array[1],
+		str_array[2], str_array[3], str_array[4]);
 	free(str_array[0]);
 	free(str_array[1]);
 	free(str_array[2]);
 	free(str_array[3]);
+	free(str_array[4]);
 	free(str_array);
-}
-*/
+}*/
+
