f7f6dafd6dc081596a7b08799084539294904435
Ryan Boudwin <rboudwin@student.hive.fi>
2023-11-06 11:08:08 +0200

    improved memory allocation precision for ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 10:45:05 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/11/01 10:46:42 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/11/06 11:05:26 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	j = 0;
 	if (s == NULL)
 		return (NULL);
-	substring = malloc(len + 1);
-	if (substring == NULL)
-		return (NULL);
-	if (start >= len || s[0] == '\0' || len == 0)
+	if (start > ft_strlen(s) || s[0] == '\0' || len == 0)
 	{
+		substring = malloc(1);
 		substring[0] = '\0';
 		return (substring);
 	}
+	if (ft_strlen(s) > len)
+		substring = malloc(len + 1);
+	else
+		substring = malloc(ft_strlen(s) + 1 - start);
+	if (substring == NULL)
+		return (NULL);
 	while (s[i] != '\0' && j < len)
-	{
-		substring[j] = s[i];
-		i++;
-		j++;
-	}
+		substring[j++] = s[i++];
 	substring[j] = '\0';
 	return (substring);
 }
