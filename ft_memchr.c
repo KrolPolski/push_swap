be21188cb7de503a613e50c62585c396c4e02946
Ryan Boudwin <rboudwin@student.hive.fi>
2023-11-06 09:52:29 +0200

    ft_memchr.c now passing all three tests

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 10:21:52 by rboudwin          #+#    #+#             */
-/*   Updated: 2023/11/02 14:31:06 by rboudwin         ###   ########.fr       */
+/*   Updated: 2023/11/06 09:50:21 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -22,8 +22,10 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	if (n == 0)
 		return (NULL);
 	str = (unsigned char *)s;
-	while (str[i] != '\0' && str[i] != d && i < n - 1)
+	while (str[i] != d && i < n - 1)
+	{
 		i++;
+	}
 	if (str[i] == d)
 		return (&str[i]);
 	else
@@ -33,7 +35,14 @@ void	*ft_memchr(const void *s, int c, size_t n)
 #include <stdio.h>
 int	main(void)
 {
-	char string[] = "So long and thanks";
+	char s[] = {0, 1, 2, 3, 4, 5};
+	char *str;
+
+	str = ft_memchr(s, 2, 3);
+	printf("'%c'\n", s[2]);
+	printf("The result is '%s'", str);
+*/
+/*	char string[] = "So long and thanks";
 	char str[] = "bonjourno";
 	char search = 'l';
 	void *ptr;
@@ -43,5 +52,5 @@ int	main(void)
 	search = 'n';
 	ptr = ft_memchr(str, search, 2);
 	printf("The character '%c' is found in '%s' at '%p'\n", search, str, ptr);
-
-}*/
+*/
+//}
