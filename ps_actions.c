c8bd12e2f7c1e5174fdbd1cf990fb130af351719
Ryan Boudwin <rboudwin@student.hive.fi>
2024-01-04 12:00:19 +0200

    sa implemented and tested; pb can run once but seg faults when it runs twice

--- ps_actions.c
+++ ps_actions.c
@@ -1,35 +1,73 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   ps_actions.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/28 13:15:40 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/01/04 11:51:31 by rboudwin         ###   ########.fr       */
+/*   Created: 2024/01/04 11:22:05 by rboudwin          #+#    #+#             */
+/*   Updated: 2024/01/04 11:51:04 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# include "Libft/libft.h"
+#include "push_swap.h"
+
 /*sa (swap a): Swap the first 2 elements at the top of stack a.
 Do nothing if there is only one or no elements.*/
-int	sa(t_vec *a);
+int	sa(t_vec *a)
+{
+	int *ptr_0;
+	int *ptr_1;
+	int	tmp;
+	ptr_0 = vec_get(a, 0);
+	ptr_1 = vec_get(a, 1);
+	ft_printf("sa\n");
+	ft_printf("0 and 1 positions are '%d' and '%d' respectively\n", *ptr_0, *ptr_1);
+	tmp = *ptr_1;
+	*ptr_1 = *ptr_0;
+	*ptr_0 = tmp;
+	ft_printf("0 and 1 positions are now '%d' and '%d' respectively\n", *ptr_0, *ptr_1);
+}
 
 /* sb (swap b): Swap the first 2 elements at the top of stack b.
 Do nothing if there is only one or no elements.*/
-int	sb(t_vec *b);
+int	sb(t_vec *b)
+{
+	int *ptr_0;
+	int *ptr_1;
+	int	tmp;
+	ptr_0 = vec_get(b, 0);
+	ptr_1 = vec_get(b, 1);
+	ft_printf("sb\n");
+	ft_printf("0 and 1 positions are '%d' and '%d' respectively\n", *ptr_0, *ptr_1);
+	tmp = *ptr_1;
+	*ptr_1 = *ptr_0;
+	*ptr_0 = tmp;
+	ft_printf("0 and 1 positions are now '%d' and '%d' respectively\n", *ptr_0, *ptr_1);
+}
 
 /* ss : sa and sb at the same time.*/
-int ss(t_vec *a, t_vec *b);
+int ss(t_vec *a, t_vec *b)
+{
+	sa(a);
+	sb(b);
+}
 
 /* pa (push a): Take the first element at the top of b and put it at the top of a.
 Do nothing if b is empty.*/
-int	pa(t_vec *a, t_vec *b);
+int	pa(t_vec *a, t_vec *b)
+{
+	vec_insert(a, vec_get(b, 0), 0);
+	vec_remove(b, 0);
+}
 
 /* pb (push b): Take the first element at the top of a and put it at the top of b.
 Do nothing if a is empty.*/
-int	pb(t_vec *a, t_vec *b);
+int	pb(t_vec *a, t_vec *b)
+{
+	vec_insert(b, vec_get(a, 0), 0);
+	vec_remove(a, 0);
+}
 
 /* ra (rotate a): Shift up all elements of stack a by 1.
 The first element becomes the last one.*/
@@ -52,6 +90,3 @@ int	rrb(t_vec *b);
 
 /*rrr : rra and rrb at the same time.*/
 int	rrr(t_vec *a, t_vec *b);
-
-int	print_vector(t_vec *vec);
-#endif
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 13:15:33 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/01/04 11:13:01 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/01/04 11:59:51 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@
 	5. Write a function for each push_swap instruction
 	6. Write logic to decide how to choose which instruction for what situation
 */
+
+/* Parse the arguments and add them as elements to the vector */
 int	convert_and_build_vector(t_vec *a, int argc, char **argv)
 {
 	int	tmp;
@@ -37,21 +39,54 @@ int	convert_and_build_vector(t_vec *a, int argc, char **argv)
 		tmp = ft_atoi(argv[i]);
 		vec_push(a, &tmp);
 		ptr = vec_get(a, i - 1);
-		ft_printf("%d, ", *ptr);
+		//ft_printf("%d, ", *ptr);
 		i++;
 	}
 	return (1);
+}
+
+int	push_swap_small(t_vec *a, t_vec *b)
+{
+	
+}
+
+int	push_swap(t_vec *a, t_vec *b)
+{
+	sa(a);
+	pb(a, b);
+	//pb(a, b);
+	ft_printf("a and b are now: \n");
+	print_vector(a);
+	print_vector(b);
+	//sb(b);
+	ft_printf("a and b are now: \n");
+	print_vector(a);
+	print_vector(b);
+	//if (a->len < 4)
+	//	push_swap_small(a, b);
+	/*if (a->len < 10)
+		push_swap_medium(a, b);
+	if (a->len < 100)
+		push_swap_large(a, b);
+	if (a->len >= 100)
+		push_swap_jumbo(a, b);*/
 }
 int	main(int argc, char **argv)
 {
 	int ret;
 	t_vec a;
+	t_vec b;
 
 	if (argc < 2)
 		ft_putstr_fd("Error\n", 2);
 	vec_new(&a, 1, sizeof(int));
+	vec_new(&b, 1, sizeof(int));
 	ret = convert_and_build_vector(&a, argc, argv);
+	print_vector(&a);
 	if (ret == -1)
 		ft_putstr_fd("Error\n", 2);
+	//if we get here we can assume arguments are clean
+	push_swap(&a, &b);
 	vec_free(&a);
+	vec_free(&b);
 }
